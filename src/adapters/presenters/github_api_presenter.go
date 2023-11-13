2224dadae60d88a2c1345dccde6e1e30e9205c9c
sikigasa <km3t1114@yahoo.co.jp>
2023-11-13 22:43:44 +0900

    fix:typo

---
+++ src/adapters/presenters/github_api_presenter.go
@@ -0,0 +1,17 @@
+package presenters
+
+import "github.com/Nagoya-Caravan-Hackathon-PDD/backend/src/datastructure/output"
+
+type githubAPIPresenter struct {
+}
+
+func NewGithubAPIPresenter() *githubAPIPresenter {
+	return &githubAPIPresenter{}
+}
+
+func (g *githubAPIPresenter) GithubAPI(err error) (int, output.GithubAPIResponse) {
+	if err != nil {
+		return 500, output.GithubAPIResponse{}
+	}
+	return 200, output.GithubAPIResponse{}
+}
